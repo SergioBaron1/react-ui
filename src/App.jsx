b062561c7e87c2807bf3f542e620c2f28ecc9c5c
Sergio Baron <sbaronrivera@gmail.com>
2025-04-25 21:10:56 -0500

    Added some details to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,6 @@ function App() {
             <Route path="/Usuarios" element={<Usuarios />} />
             <Route path="/Logout" element={<Logout />} />
         </Routes>
-        {/*<div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">*/}
-        {/*    <h1 className="text-4xl font-bold text-blue-600">¡Tailwind está funcionando!</h1>*/}
-        {/*</div>*/}
     </BrowserRouter>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,20 @@
 import { NavLink } from 'react-router-dom';
-
+import imageLogo from '../assets/management.png';
 export default function Navbar(){
 
     return (
         <nav className="bg-sky-900 p-4 text-white flex items-center justify-between">
-            <div className="font-bold text-xl">
-                Panel administrativo de control
+            <div className="flex items-center justify-between">
+                <img className="h-11 w-auto object-contain mr-4 " src={imageLogo}/>
+                <h1 className="font-bold text-xl hidden md:block">
+                    Panel administrativo de control
+                </h1>
             </div>
-            <div className="space-x-4">
-                <NavLink to="/Clientes" className="hover:underline">Clientes</NavLink>
-                <NavLink to="/Proveedor" className="hover:underline">Proveedor</NavLink>
-                <NavLink to="/Usuarios" className="hover:underline">Usuarios</NavLink>
-                <NavLink to="/Logout" className="hover:underline">Logout</NavLink>
+            <div className="space-x-4 flex items-center">
+                <NavLink to="/Clientes" className="hover:backdrop-saturate-100 px-2 py-1 rounded hover:bg-sky-800 transition-colors">Clientes</NavLink>
+                <NavLink to="/Proveedor" className="hover:backdrop-saturate-100 px-2 py-1 rounded hover:bg-sky-800 transition-colors">Proveedor</NavLink>
+                <NavLink to="/Usuarios" className="hover:backdrop-saturate-100 px-2 py-1 rounded hover:bg-sky-800 transition-colors">Usuarios</NavLink>
+                <NavLink to="/Logout" className="hover:backdrop-saturate-100 px-2 py-1 rounded hover:bg-rose-800 transition-colors">Logout</NavLink>
             </div>
         </nav>
     );

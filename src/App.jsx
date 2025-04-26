a3c2b41db3b6d39e19df348aeb58112cadb3f19e
Sergio Baron <sbaronrivera@gmail.com>
2025-04-25 19:40:32 -0500

    Configuración inicial

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ function App() {
 
   return (
     <>
+        <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold text-blue-600">¡Tailwind está funcionando!</h1>
+        </div>
+
         <Component />
     </>
   )
